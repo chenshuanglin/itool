ac35ab6859cc43406ee4bd983550d9362617b015
chenshuanglin <1026624591@qq.com>
2017-11-19 22:55:58 +0800

    稍微修改下，还没完善

--- requester.go
+++ requester.go
@@ -13,7 +13,7 @@ import (
 
 //持续迭代数组中的IP地址
 func ip_generate(ips []string) chan string {
-	out := make(chan string)
+	out := make(chan string, 1024)
 
 	go func() {
 		i := 0
@@ -33,9 +33,43 @@ func ip_generate(ips []string) chan string {
 //获取随机端口
 func rand_generate() chan int {
 	out := make(chan int, 1024)
+	rand.Seed(time.Now().UnixNano())
 	go func() {
-		rand.Seed(time.Now().UnixNano())
-		out <- rand.Intn(65534-1024) + 1024
+		for {
+			out <- rand.Intn(65534-1024) + 1024
+		}
+	}()
+	return out
+}
+
+//获取客户端连接服务
+func (w *Work) http_client() chan *http.Client {
+
+	out := make(chan *http.Client, 1024)
+	go func() {
+		for {
+			//获取源地址
+			localAddr, err := net.ResolveIPAddr("ip", <-w.ip)
+			if err != nil {
+				panic(err)
+			}
+
+			localTCPAddr := net.TCPAddr{
+				IP:   localAddr.IP,
+				Port: <-w.port,
+			}
+
+			tr := &http.Transport{
+				Dial:              (&net.Dialer{LocalAddr: &localTCPAddr, Timeout: 2 * time.Second}).Dial,
+				DisableKeepAlives: true,
+			}
+
+			c := &http.Client{
+				Transport: tr,
+			}
+
+			out <- c
+		}
 	}()
 	return out
 }
@@ -53,20 +87,26 @@ type Work struct {
 	//源地址数组
 	ipaddrs []string
 
+	//多久时间打印一次结果
+	time time.Duration
+
 	//当前源地址
 	ip chan string
 
 	//请求的源端口
 	port chan int
 
-	//多久时间打印一次结果
-	time time.Duration
+	//当前请求客户端
+	c chan *http.Client
 
 	//请求的url
 	url string
 
 	//失败次数累加和
-	failedNumber int
+	failedNumber int64
+
+	//请求次数累加和
+	requestNum int64
 }
 
 func (w *Work) Run() {
@@ -74,6 +114,7 @@ func (w *Work) Run() {
 
 	w.ip = ip_generate(w.ipaddrs)
 	w.port = rand_generate()
+	w.c = w.http_client()
 
 	go func() {
 		w.runWorker()
@@ -83,27 +124,29 @@ loop:
 	for {
 		select {
 		case <-tick:
+			fmt.Println("当前请求总数:", w.requestNum)
 			fmt.Println("当前失败数:", w.failedNumber)
 		case <-w.stop:
 			fmt.Println("Good bye\n")
 			close(w.done)
+			time.Sleep(1 * time.Second)
 			break loop
 		}
 	}
 }
 
 func (w *Work) runWorker() {
-	var throttle <-chan time.Time
-
 	//计算多久发出一次请求
-	throttle = time.Tick(time.Duration(1e6/w.n) * time.Microsecond)
+	tick := time.NewTicker(time.Duration(1e6/w.n) * time.Microsecond)
 loop:
 	for {
 		select {
-		case <-throttle:
+		case <-tick.C:
+			w.requestNum++
 			//发起一次请求
 			go w.makeRequest()
 		case <-w.done:
+			tick.Stop()
 			fmt.Println("Stop request\n")
 			break loop
 		}
@@ -111,10 +154,14 @@ loop:
 }
 
 func (w *Work) makeRequest() {
-	c := w.getHttpClient()
+
+	c := <-w.c
 	resp, err := c.Get(w.url)
 	if err != nil {
 		w.failedNumber++
+		go func() {
+			fmt.Println(err)
+		}()
 		return
 	}
 
@@ -123,9 +170,6 @@ func (w *Work) makeRequest() {
 }
 
 func (w *Work) getHttpClient() *http.Client {
-	go func() {
-		fmt.Println("卡在这了")
-	}()
 	//获取源地址
 	localAddr, err := net.ResolveIPAddr("ip", <-w.ip)
 	if err != nil {
@@ -139,7 +183,7 @@ func (w *Work) getHttpClient() *http.Client {
 
 	tr := &http.Transport{
 		Dial:              (&net.Dialer{LocalAddr: &localTCPAddr, Timeout: 2 * time.Second}).Dial,
-		DisableKeepAlives: false,
+		DisableKeepAlives: true,
 	}
 
 	c := &http.Client{
